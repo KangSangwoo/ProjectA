237061e7dda9c3e5ec15b9ddc4c1e04185cd0321
03211202 <nightcity6348@gmail.com>
2021-12-30 17:53:13 +0900

    1

---
+++ KSW_2D/Assets/Script/PlayerMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        this.transform.position = transform.Translate(Vector3.right * Time.deltaTime);
+            
+            
+    }
+}
