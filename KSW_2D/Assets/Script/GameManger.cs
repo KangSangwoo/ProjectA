71ee8628a1a3dbe6fd98c8ebac5b0d883b53d1e9
03211202 <nightcity6348@gmail.com>
2022-01-04 17:49:16 +0900

    0104

---
+++ KSW_2D/Assets/Script/GameManger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManger : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+/*
+public void OnClick_UnitButton()
+{
+    GameObject enermy = Instantiate(_Unit1Source); // 유닛 생성
+    enermy.SetActive(true);             //  적 활성화
+    this.transform.position = new Vector3(-6, 2, 0);    // 윗줄생산
+
+}
+*/
--- KSW_2D/Assets/Script/PlayerMove.cs
+++ KSW_2D/Assets/Script/PlayerMove.cs
@@ -1,21 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerMove : MonoBehaviour
 {
     public float _speed = 1.0f;
 
+    public GameObject _Unit1Source = null;        // 유닛1 리소스
+
+    public Transform _CreatePosition;     //생성 위치
+
+    public GameObject _UnitButton;      // 유닛버튼
+
+
     void Start()
     {
-        this.transform.position = new Vector3(-6, 2, 0);    // 윗줄생산
+       // this.transform.position = new Vector3(-6, 2, 0);    // 윗줄생산
 
     }
 
     void Update()
     {
-
-        this.transform.Translate(Vector3.right * _speed * Time.deltaTime);
-
+        if (Input.GetButtonDown("Fire1"))
+        {
+            this.transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
     }
 }
+
+/*
+public void OnClick_UnitButton()
+{
+    GameObject enermy = Instantiate(_Unit1Source); // 유닛 생성
+    enermy.SetActive(true);             //  적 활성화
+    this.transform.position = new Vector3(-6, 2, 0);    // 윗줄생산
+
+}
+*/
